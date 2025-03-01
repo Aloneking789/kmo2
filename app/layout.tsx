d16049eddf0746dbe56e581c4a146063c1635c38
Abhay Pandey <116704975+Aloneking789@users.noreply.github.com>
2025-03-01 19:00:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'KMO',
+  description: 'Made by Elite with ❤️',
+  generator: 'Elite',
 }
 
 export default function RootLayout({
